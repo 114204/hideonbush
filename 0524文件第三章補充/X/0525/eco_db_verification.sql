0fa5f3fca9277c1dc73d6ef5e5fb79a0659ee327
Luna070168 <11336026@ntub.edu.tw>
2025-05-25 14:05:55 +0800

    Add files via upload

---
+++ 0524文件第三章補充/X/0525/eco_db_verification.sql
@@ -0,0 +1,60 @@
+-- MySQL dump 10.13  Distrib 8.0.41, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: eco_db
+-- ------------------------------------------------------
+-- Server version	8.0.41
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `verification`
+--
+
+DROP TABLE IF EXISTS `verification`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `verification` (
+  `verificationId` int NOT NULL AUTO_INCREMENT,
+  `recordId` int NOT NULL,
+  `verifierId` int NOT NULL,
+  `result` tinyint(1) DEFAULT NULL,
+  `note` varchar(200) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `create_time` datetime DEFAULT CURRENT_TIMESTAMP,
+  `update_time` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`verificationId`),
+  KEY `recordId` (`recordId`),
+  KEY `verifierId` (`verifierId`),
+  CONSTRAINT `verification_ibfk_1` FOREIGN KEY (`recordId`) REFERENCES `recyclerecord` (`recordId`),
+  CONSTRAINT `verification_ibfk_2` FOREIGN KEY (`verifierId`) REFERENCES `verifier` (`verifierId`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `verification`
+--
+
+LOCK TABLES `verification` WRITE;
+/*!40000 ALTER TABLE `verification` DISABLE KEYS */;
+INSERT INTO `verification` VALUES (1,1,1,1,'正確分類','2025-05-23 23:23:00','2025-05-24 03:20:00');
+/*!40000 ALTER TABLE `verification` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-05-24 22:04:53
